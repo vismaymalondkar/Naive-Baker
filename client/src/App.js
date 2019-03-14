abd734d0df6f744dafa87249d380bc075117bf8e
Smit Patel <smitpatel@Smits-MacBook-Pro.local>
2019-03-14 12:34:29 +0530

    Database

--- client/src/App.js
+++ client/src/App.js
@@ -21,65 +21,16 @@ class App extends Component {
       "Spinach"
     ]
   };
-
-  // componentDidMount() {
-  //   this.callApi()
-  //     .then(res => this.setState({ response: res.express }))
-  //     .catch(err => console.log(err));
-  //     //var temp=this.state.suggestions;
-  //     //temp.sort();
-  //     //console.log(temp);
-  //     //this.setState({suggestions:temp});
-  //     console.log(this.state.responseToPost);
-
-  // }
-
-  // callApi = async () => {
-  //   const response = await fetch('/users');
-  //   console.log(response);
-  //   const body = await response.json();
-  //   console.log(body);
-  //   if (response.status !== 200) throw Error(body.message);
-
-  //   return body;
-  // };
-
-  // handleSubmit = async e => {
-  //   e.preventDefault();
-  //   const response = await fetch('/users', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ post: this.state.post }),
-  //   });
-  //   const body = await response.text();
-
-  //   this.setState({ responseToPost: body });
-  // };
+  // Add suggestions array
   render() {
     return (
       <BrowserRouter>
+      
+      <div className="App">
 
-        <div className="App">
-
-          <NaiveBaker />
-         
-           {/* <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>  */}
-        </div>
-   </BrowserRouter>
+        <NaiveBaker />
+      </div>
+    </BrowserRouter>
     );
   }
 }
